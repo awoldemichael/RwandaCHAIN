f86c39efbd66f2850d05f07c333f541aefad2877
flaneuse <laura.d.hughes@gmail.com>
2016-06-06 15:11:12 -0400

    set up Shiny dashboard

---
+++ rwandaCHAIN/global.R
@@ -0,0 +1,18 @@
+# 
+# Laura Hughes, lhughes@usaid.gov, 6 June 2016
+
+# Set up the app ----------------------------------------------------------
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(shiny)
+library(shinydashboard)
+library(stringr)
+library(llamar)
+library(choroplethr)
+library(choroplethrMaps)
+library(RColorBrewer)
+
+
+# import data -------------------------------------------------------------
+
