1f34ade95f455aeb7d3f4ffe08a9550238bd0ecb
flaneuse <laura.d.hughes@gmail.com>
2016-06-15 20:42:59 -0400

    playing w/ leaflet

--- R/rw_leaflet.R
+++ R/rw_leaflet.R
@@ -3,20 +3,41 @@ library(leaflet)
 setwd('~/Documents/USAID/Rwanda/data in/Rwanda_Admin2/')
 rw = readOGR(dsn=".", layer="District_Boundary_2006")
 
-rw@data = rw@data %>% mutate(x = dense_rank(Dist_ID))
+rw@data = full_join(rw@data, filteredDF)
 
-pal <- colorQuantile("YlGn", NULL, n = 5)
+pal <- colorNumeric("YlGnBu", domain = NULL)
+
 
 state_popup <- paste0("<strong>Province: </strong>", 
                       rw$Prov_Name, 
                       "<br><strong>District: </strong>", 
-                      rw$District)
+                      rw$District,
+                      "<br><strong>number: </strong>", 
+                      rw$num)
+
+
+spacer = 0.01
+
+minLon = rw@bbox['x', 'min'] * (1 - spacer)
+minLat = rw@bbox['y', 'min'] * (1 - spacer)
+maxLon = rw@bbox['x', 'max'] * (1 + spacer)
+maxLat = rw@bbox['y', 'max'] * (1 + spacer)
+
 
 leaflet(data = rw) %>%
-  addProviderTiles("CartoDB.Positron") %>% 
-  addPolygons(fillColor = ~pal(-x), 
+  addProviderTiles("Esri.WorldGrayCanvas") %>% 
+  addPolygons(fillColor = ~pal(num), 
               fillOpacity = 0.8, 
               color = "#BDBDC3", 
               weight = 1,
               popup = state_popup)
 
+rw@data = rw@data %>% filter(subIR_ID %like% '3')
+
+leaflet(data = rw) %>%
+  addProviderTiles("Esri.WorldGrayCanvas") %>% 
+  addPolygons(fillColor = ~pal(num), 
+              fillOpacity = 0.8, 
+              color = "#BDBDC3", 
+              weight = 1,
+              popup = state_popup)
