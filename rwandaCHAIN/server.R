0ba216ef775cd0383d4c5d57c50d3fb4f0967eda
flaneuse <laura.d.hughes@gmail.com>
2016-06-18 15:38:43 -0400

    circles instead choro

--- rwandaCHAIN/server.R
+++ rwandaCHAIN/server.R
@@ -10,15 +10,24 @@ shinyServer(
     })
     
     
-
+    
     output$main = renderLeaflet({
       
       filteredDF = filterDF()
       
       rw_adm2@data = full_join(rw_adm2@data, filteredDF)
       
-      pal <- colorNumeric("YlGnBu", domain = NULL)
+      # -- Pull out the centroids --
+      rw_centroids = data.frame(coordinates(rw_adm2)) %>% 
+        rename(Lon = X1, Lat = X2)
       
+      rw_centroids = cbind(rw_centroids,
+                           District = rw_adm2@data$District)
+      
+      rw_centroids = full_join(rw_centroids, filteredDF)
+      
+      
+      # -- Info popup box -- 
       info_popup <- paste0("<strong>Province: </strong>", 
                            rw_adm2$Prov_Name, 
                            "<br><strong>District: </strong>", 
@@ -30,14 +39,32 @@ shinyServer(
         addProviderTiles("Esri.WorldGrayCanvas", 
                          options = tileOptions(minZoom = 8, maxZoom  = 11)) %>% 
         setMaxBounds(minLon, minLat, maxLon, maxLat) %>% 
-        addPolygons(fillColor = ~pal(num), 
+        addPolygons(fillColor = baseColour, 
                     fillOpacity = 0.8, 
                     color = "#BDBDC3", 
                     weight = 1,
-                    popup = info_popup)
+                    popup = info_popup) %>% 
+        addCircles(data = rw_centroids, lat = ~Lat, lng = ~Lon,
+                   radius = ~num*200, 
+                   color = strokeColour, weight = 0.5,
+                   fillColor = ~pal(num), fillOpacity = 1,
+                   label = ~District,
+                   labelOptions = lapply(1:nrow(rw_centroids), function(x) {
+                     labelOptions(opacity=0.9, noHide = TRUE)
+                   })
+        )
       
     })
     
     # callModule(indivRegion, 'west', df, 'West')
+    
+    
+    leaflet(rw_centroids) %>% addTiles() %>%
+      addCircleMarkers(lng = ~Lon, lat = ~Lat,
+                       label = ~District,
+                       labelOptions = lapply(1:nrow(rw_centroids), function(x) {
+                         labelOptions(opacity = 1, noHide = T)
+                       })
+      )
     
   })
