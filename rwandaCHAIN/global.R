a43c26d7f720f82575f77b40ae736a6d1d5ae96d
flaneuse <laura.d.hughes@gmail.com>
2016-06-18 18:17:28 -0400

    embiggen circles more

--- rwandaCHAIN/global.R
+++ rwandaCHAIN/global.R
@@ -58,7 +58,7 @@ categPal = colorFactor(palette = c('#e41a1c', '#377eb8',
 # sizes -------------------------------------------------------------------
 widthMap = '750px'
 heightMap = '675px'
-circleScaling = 500
+circleScaling = 1000
 
 
 # Source files ------------------------------------------------------------
