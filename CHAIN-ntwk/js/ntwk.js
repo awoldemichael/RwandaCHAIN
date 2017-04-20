931560471195dc5157749913f2385a17d9036c07
flaneuse <laura.d.hughes@gmail.com>
2017-04-20 19:33:50 -0400

    + tooltips

--- CHAIN-ntwk/js/ntwk.js
+++ CHAIN-ntwk/js/ntwk.js
@@ -6,7 +6,7 @@ var color = d3.scaleOrdinal(d3.schemeCategory20);
 
 var simulation = d3.forceSimulation()
   .force("link", d3.forceLink().id(function(d) { return d.id; }))
-  .force("charge", d3.forceManyBody().strength(-1000))
+  .force("charge", d3.forceManyBody().strength(-2250))
   .force("center", d3.forceCenter(width / 2, height / 2));
 
 d3.csv("./data/20170414_IP_nodes.csv", function(error, nodeData) {
@@ -25,7 +25,7 @@ edgeData.forEach(function(d){
   d.value = +d.n;
 });
 
-
+allIPs = nodeData.map(function(element) {return element.id});
 
   if (error) throw error;
 
@@ -34,7 +34,6 @@ edgeData.forEach(function(d){
   graph.nodes = nodeData;
   graph.links = edgeData;
 
-console.log(graph)
 
 svg.append("text")
   .attr("class", "title")
@@ -64,6 +63,19 @@ svg.append("text")
     .attr("y", 155)
     .text("Click and drag a circle to pull apart the nest");
 
+    // Define the div for the tooltip
+var divNode = d3.select("body").append("div")
+    .attr("class", "tooltip")
+    .style("width", "125px")
+    .style("height", "18px")
+    .style("opacity", 0);
+
+var divEdge = d3.select("body").append("div")
+    .attr("class", "tooltip")
+    .style("width", "50px")
+    .style("height", "18px")
+    .style("opacity", 0);
+
   var link = svg.append("g")
       .attr("class", "links")
     .selectAll("line")
@@ -71,6 +83,7 @@ svg.append("text")
     .enter().append("line")
       // .attr("stroke", function(d) { return color(d.source); })
       .attr("stroke-width", function(d) { return (d.value * 2); });
+
 
   var node = svg.append("g")
       .attr("class", "nodes")
@@ -80,28 +93,119 @@ svg.append("text")
       .attr("r", function(d) { return d.unique * 3;})
       .attr("fill", function(d) { return color(d.id); })
       .attr("opacity", 0.9)
+
       .call(d3.drag()
           .on("start", dragstarted)
           .on("drag", dragged)
           .on("end", dragended));
+
+// -- TOOLTIPS (nodes) --
+
+  node.on("mouseover", function(sel) {
+
+    linked = edgeData.filter(function(d) { return d.source.id == sel.id | d.target.id == sel.id})
+    linkedSources = linked.map(function(element) {return element.source.id});
+    linkedTargets = linked.map(function(element) {return element.target.id});
+
+    selIPs = linkedTargets.concat(linkedSources);
+
+
+    link.transition()
+      .style("opacity", 1)
+    .filter(function(d) { return d.source.id != sel.id & d.target.id != sel.id;})
+        .duration(200)
+        .style("opacity", 0.1);
+
+
+    node.transition()
+    // .filter(function(d, i) { console.log(d.id); console.log(sel.id == "AEE"); sel.id == "AEE";})
+      .filter(function(d, i) { return !selIPs.includes(d.id);})
+      .duration(200)
+      .style("opacity", 0.25)
+
+    divNode.transition()
+        .duration(200)
+        // .style("background", color(d.id))
+        // .style("background")
+        // .attr("fill", function(d) { return color(d.id); })
+        .style("opacity", 0.85);
+    // divNode.html(sel.id + " works with <br/>" + sel.unique + " partners")
+    divNode.html(sel.unique + " partnerships")
+        .style("color", color(sel.id))
+        .style("left", (d3.event.pageX) + "px")
+        .style("top", (d3.event.pageY - 28) + "px");
+    })
+.on("mouseout", function(d) {
+    divNode.transition()
+        .duration(500)
+        .style("opacity", 0);
+
+  link.transition()
+      .duration(200)
+      .style("opacity", 0.6);
+
+    node.transition()
+          .duration(500)
+          .style("opacity", 0.9)
+});
+
+
+// -- edge tooltip --
+      link.on("mouseover", function(sel) {
+  // grey out edges
+        link.transition()
+        .filter(function(d) { return d.source != sel.source | d.target != sel.target;})
+            .duration(200)
+            .style("opacity", 0.1);
+
+
+  // grey out nodes
+        node.transition()
+        .filter(function(d) { return d.id != sel.source.id & d.id != sel.target.id;})
+            .duration(200)
+            .style("opacity", 0.1);
+        //
+        // node.transition()
+        // .filter(function(d) { return d.id == sel.source.id | d.id == sel.target.id;})
+        //     .duration(200)
+        //     .style("opacity", 0.9);
+
+    // tooltip
+        divEdge.transition()
+            .duration(200)
+            .style("opacity", 0.85);
+        divEdge.html(sel.value + " tasks")
+            .style("left", (d3.event.pageX) + "px")
+            .style("top", (d3.event.pageY - 28) + "px");
+        })
+    .on("mouseout", function(d) {
+              link.transition()
+                  .duration(500)
+                  .style("opacity", 1);
+
+        divEdge.transition()
+            .duration(500)
+            .style("opacity", 0);
+
+      node.transition()
+          .duration(500)
+          .style("opacity", 0.9);
+
+      link.transition()
+          .duration(500)
+          .style("opacity", 0.6);
+    });
+
 
   var labels = svg.selectAll("#ip-labels")
   .data(graph.nodes)
   .enter().append("text")
     .attr("id", "ip-labels")
     .style("font-size", "15px")
-    // .append("text")
-        // .attr("class", "source")
-        // .attr("id", "email")
-        // .attr("x", function(d) { console.log(d.x); return d.x; })
-        // .attr("y", function(d) { return d.y; })
-        // .attr("y", 100)
-        // .attr("x", 100)
         .style("text-anchor", "middle")
         .style("alignment-baseline", "middle")
-        // .text("geocenter@usaid.gov")
         .style("fill", "black")
-        .style("z-index", 1000)
+        .style("z-index", 0)
       .text(function(d) { return d.id; });
 
   simulation
